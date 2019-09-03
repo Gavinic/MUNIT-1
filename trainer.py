eb6b3aa8582846c6fba9e1bac437c13f1fe74f49
Richard Tseng <Gavinzmail@163.com>
2019-09-03 14:42:14 +0800

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -1,3 +1,4 @@
+# -*- coding=utf-8 -*-
 """
 Copyright (C) 2017 NVIDIA Corporation.  All rights reserved.
 Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
@@ -11,36 +12,42 @@ import os
 
 class MUNIT_Trainer(nn.Module):
     def __init__(self, hyperparameters):
-        super(MUNIT_Trainer, self).__init__()
+        super(MUNIT_Trainer, self).__init__()   # super() 函数是用于调用父类(超类)的一个方法。
         lr = hyperparameters['lr']
-        # Initiate the networks
+        # Initiate the networks， 需要好好看看生成器和鉴别器到底是如何构造的
         self.gen_a = AdaINGen(hyperparameters['input_dim_a'], hyperparameters['gen'])  # auto-encoder for domain a
         self.gen_b = AdaINGen(hyperparameters['input_dim_b'], hyperparameters['gen'])  # auto-encoder for domain b
         self.dis_a = MsImageDis(hyperparameters['input_dim_a'], hyperparameters['dis'])  # discriminator for domain a
         self.dis_b = MsImageDis(hyperparameters['input_dim_b'], hyperparameters['dis'])  # discriminator for domain b
+        # https://blog.csdn.net/liuxiao214/article/details/81037416
         self.instancenorm = nn.InstanceNorm2d(512, affine=False)
         self.style_dim = hyperparameters['gen']['style_dim']
-
         # fix the noise used in sampling
         display_size = int(hyperparameters['display_size'])
-        self.s_a = torch.randn(display_size, self.style_dim, 1, 1).cuda()
+        # s_a , s_b 表示的是两个不同的style
+        self.s_a = torch.randn(display_size, self.style_dim, 1, 1).cuda()  # 16*8*1*1
         self.s_b = torch.randn(display_size, self.style_dim, 1, 1).cuda()
 
         # Setup the optimizers
         beta1 = hyperparameters['beta1']
         beta2 = hyperparameters['beta2']
+        # 两个鉴别器
         dis_params = list(self.dis_a.parameters()) + list(self.dis_b.parameters())
+        # 两个生成器
         gen_params = list(self.gen_a.parameters()) + list(self.gen_b.parameters())
+        # 优化器
         self.dis_opt = torch.optim.Adam([p for p in dis_params if p.requires_grad],
                                         lr=lr, betas=(beta1, beta2), weight_decay=hyperparameters['weight_decay'])
         self.gen_opt = torch.optim.Adam([p for p in gen_params if p.requires_grad],
                                         lr=lr, betas=(beta1, beta2), weight_decay=hyperparameters['weight_decay'])
+        # 优化策略
         self.dis_scheduler = get_scheduler(self.dis_opt, hyperparameters)
         self.gen_scheduler = get_scheduler(self.gen_opt, hyperparameters)
 
         # Network weight initialization
-        self.apply(weights_init(hyperparameters['init']))
-        self.dis_a.apply(weights_init('gaussian'))
+        # 解释 apply  apply(lambda x,y : x+y, (1),{'y' : 2})   https://zhuanlan.zhihu.com/p/42756654
+        self.apply(weights_init(hyperparameters['init']))  # 初始化当前类
+        self.dis_a.apply(weights_init('gaussian'))   # 初始化dis_a,是一个类对象
         self.dis_b.apply(weights_init('gaussian'))
 
         # Load VGG model if needed
@@ -55,23 +62,31 @@ class MUNIT_Trainer(nn.Module):
 
     def forward(self, x_a, x_b):
         self.eval()
-        s_a = Variable(self.s_a)
-        s_b = Variable(self.s_b)
-        c_a, s_a_fake = self.gen_a.encode(x_a)
+        s_a = Variable(self.s_a)     # here the self.s_a is random style
+        s_b = Variable(self.s_b)     # here the self.s_b is random style
+        # 两个auto-encoder
+        c_a, s_a_fake = self.gen_a.encode(x_a)  # c_a, s_a_fake is the content and style of input x_a
         c_b, s_b_fake = self.gen_b.encode(x_b)
-        x_ba = self.gen_a.decode(c_b, s_a)
-        x_ab = self.gen_b.decode(c_a, s_b)
+        # x_ba 表示的是 imgb->imga
+        x_ba = self.gen_a.decode(c_b, s_a)  # combine(c_b, s_a) to generate the x_ba
+        x_ab = self.gen_b.decode(c_a, s_b)  # combine(c_a, s_b) to generate the x_ab
+        #  训练模式
         self.train()
         return x_ab, x_ba
 
     def gen_update(self, x_a, x_b, hyperparameters):
+        # 这里只负责参数的更新操作,真正的训练操作实际上来源于AdainGan 以及 MsImgDis
+        # self.gen_opt 表示的是生成器的优化器
         self.gen_opt.zero_grad()
         s_a = Variable(torch.randn(x_a.size(0), self.style_dim, 1, 1).cuda())
         s_b = Variable(torch.randn(x_b.size(0), self.style_dim, 1, 1).cuda())
         # encode
         c_a, s_a_prime = self.gen_a.encode(x_a)
         c_b, s_b_prime = self.gen_b.encode(x_b)
-        # decode (within domain)
+
+        # print('content shape:', c_b.shape)
+        # print('style shape:', s_b_prime.shape)
+        # decode (within domain), 这个是必要的部分，因为需要保证解码器的效果
         x_a_recon = self.gen_a.decode(c_a, s_a_prime)
         x_b_recon = self.gen_b.decode(c_b, s_b_prime)
         # decode (cross domain)
@@ -79,7 +94,7 @@ class MUNIT_Trainer(nn.Module):
         x_ab = self.gen_b.decode(c_a, s_b)
         # encode again
         c_b_recon, s_a_recon = self.gen_a.encode(x_ba)
-        c_a_recon, s_b_recon = self.gen_b.encode(x_ab)
+        c_a_recon, s_b_recon = self.gen_b.encode(x_ab)   # 本质上这里传过来的还是c_a， 我们希望c_a_recon与c_a越相似越好
         # decode again (if needed)
         x_aba = self.gen_a.decode(c_a_recon, s_a_prime) if hyperparameters['recon_x_cyc_w'] > 0 else None
         x_bab = self.gen_b.decode(c_b_recon, s_b_prime) if hyperparameters['recon_x_cyc_w'] > 0 else None
@@ -96,7 +111,8 @@ class MUNIT_Trainer(nn.Module):
         # GAN loss
         self.loss_gen_adv_a = self.dis_a.calc_gen_loss(x_ba)
         self.loss_gen_adv_b = self.dis_b.calc_gen_loss(x_ab)
-        # domain-invariant perceptual loss
+        # domain-invariant perceptual loss  为什么要用vgg呢？也就是内容不变的约束
+        # 对feature map 的 L2 loss  好好理解一下这里的域不变loss
         self.loss_gen_vgg_a = self.compute_vgg_loss(self.vgg, x_ba, x_b) if hyperparameters['vgg_w'] > 0 else 0
         self.loss_gen_vgg_b = self.compute_vgg_loss(self.vgg, x_ab, x_a) if hyperparameters['vgg_w'] > 0 else 0
         # total loss
@@ -123,13 +139,18 @@ class MUNIT_Trainer(nn.Module):
         return torch.mean((self.instancenorm(img_fea) - self.instancenorm(target_fea)) ** 2)
 
     def sample(self, x_a, x_b):
+        # 模型调整为eval模式
         self.eval()
+        # # random style
         s_a1 = Variable(self.s_a)
         s_b1 = Variable(self.s_b)
         s_a2 = Variable(torch.randn(x_a.size(0), self.style_dim, 1, 1).cuda())
         s_b2 = Variable(torch.randn(x_b.size(0), self.style_dim, 1, 1).cuda())
         x_a_recon, x_b_recon, x_ba1, x_ba2, x_ab1, x_ab2 = [], [], [], [], [], []
+        # 实际上x_a.size(0) = display_size  16
         for i in range(x_a.size(0)):
+            # https://blog.csdn.net/xiexu911/article/details/80820028
+            # unsequeeze 在指定的维度上对数据的维度进行扩展
             c_a, s_a_fake = self.gen_a.encode(x_a[i].unsqueeze(0))
             c_b, s_b_fake = self.gen_b.encode(x_b[i].unsqueeze(0))
             x_a_recon.append(self.gen_a.decode(c_a, s_a_fake))
@@ -138,6 +159,8 @@ class MUNIT_Trainer(nn.Module):
             x_ba2.append(self.gen_a.decode(c_b, s_a2[i].unsqueeze(0)))
             x_ab1.append(self.gen_b.decode(c_a, s_b1[i].unsqueeze(0)))
             x_ab2.append(self.gen_b.decode(c_a, s_b2[i].unsqueeze(0)))
+
+        # 这里用到了cat，那么x_a_recon是4个channel,这样最终输出的就是32个channel,然后我们将这32个channel 分别打印show出来。
         x_a_recon, x_b_recon = torch.cat(x_a_recon), torch.cat(x_b_recon)
         x_ba1, x_ba2 = torch.cat(x_ba1), torch.cat(x_ba2)
         x_ab1, x_ab2 = torch.cat(x_ab1), torch.cat(x_ab2)
@@ -145,31 +168,43 @@ class MUNIT_Trainer(nn.Module):
         return x_a, x_a_recon, x_ab1, x_ab2, x_b, x_b_recon, x_ba1, x_ba2
 
     def dis_update(self, x_a, x_b, hyperparameters):
+        # self.dis_opt 表示的是鉴别器的优化器
         self.dis_opt.zero_grad()
+        # s_a : a 图片的风格
+        # s_b : b 图片的风格
         s_a = Variable(torch.randn(x_a.size(0), self.style_dim, 1, 1).cuda())
         s_b = Variable(torch.randn(x_b.size(0), self.style_dim, 1, 1).cuda())
         # encode
+        #  content encode ,encode 只是将不同图片的内容和风格进行编码，不做迁移处理， 生成器有一个编码器和一个解码器
         c_a, _ = self.gen_a.encode(x_a)
         c_b, _ = self.gen_b.encode(x_b)
         # decode (cross domain)
-        x_ba = self.gen_a.decode(c_b, s_a)
+        # 进行交叉域的内容和风格decode,这里需要好好理解一下decode到底在做什么，为什么可以进行内容和风格的混合
+        x_ba = self.gen_a.decode(c_b, s_a)   # xba 是由随机风格和原始图像内容组合的结果
         x_ab = self.gen_b.decode(c_a, s_b)
-        # D loss
-        self.loss_dis_a = self.dis_a.calc_dis_loss(x_ba.detach(), x_a)
-        self.loss_dis_b = self.dis_b.calc_dis_loss(x_ab.detach(), x_b)
+        # D loss  鉴别器loss
+        self.loss_dis_a = self.dis_a.calc_dis_loss(x_ba.detach(), x_a)  # 这里要把x_ba, x_a 独立出来来看，因为鉴别器的目的是
+        # 为了判断真伪, self.loss_dis_a 是分别计算x_ba, x_a的loss的和， 他们两个对象之间不涉及对比。
+        # 这里挺有意思的，鉴别器的loss是两个独立输入图片的二分类loss的和, 注意的一点就是，我们实际上是希望送入鉴别器的这两张图拒用相同的风格。
+        # 这里就要区分和重建loss的区别。
+        self.loss_dis_b = self.dis_b.calc_dis_loss(x_ab.detach(), x_b) # 为什么是这样的组合呢?
+        #  hyperparameters['gan_w'] weight of adversarial loss
         self.loss_dis_total = hyperparameters['gan_w'] * self.loss_dis_a + hyperparameters['gan_w'] * self.loss_dis_b
-        self.loss_dis_total.backward()
-        self.dis_opt.step()
+        # https://blog.csdn.net/jacke121/article/details/82995740 深入理解backward(), step()
+        self.loss_dis_total.backward()  # calculate grad
+        self.dis_opt.step()         # update grad
 
     def update_learning_rate(self):
         if self.dis_scheduler is not None:
-            self.dis_scheduler.step()
+            self.dis_scheduler.step() # update the learning rate
         if self.gen_scheduler is not None:
             self.gen_scheduler.step()
 
     def resume(self, checkpoint_dir, hyperparameters):
         # Load generators
+        # 每次选择的都是最新的模型
         last_model_name = get_model_list(checkpoint_dir, "gen")
+        print('resume model: ', last_model_name)
         state_dict = torch.load(last_model_name)
         self.gen_a.load_state_dict(state_dict['a'])
         self.gen_b.load_state_dict(state_dict['b'])
@@ -189,14 +224,21 @@ class MUNIT_Trainer(nn.Module):
         print('Resume from iteration %d' % iterations)
         return iterations
 
-    def save(self, snapshot_dir, iterations):
+    def save(self, snapshot_dir, iterations, gpuids):
         # Save generators, discriminators, and optimizers
         gen_name = os.path.join(snapshot_dir, 'gen_%08d.pt' % (iterations + 1))
         dis_name = os.path.join(snapshot_dir, 'dis_%08d.pt' % (iterations + 1))
         opt_name = os.path.join(snapshot_dir, 'optimizer.pt')
-        torch.save({'a': self.gen_a.state_dict(), 'b': self.gen_b.state_dict()}, gen_name)
-        torch.save({'a': self.dis_a.state_dict(), 'b': self.dis_b.state_dict()}, dis_name)
-        torch.save({'gen': self.gen_opt.state_dict(), 'dis': self.dis_opt.state_dict()}, opt_name)
+        if len(gpuids) > 1:
+            torch.save({'a': self.gen_a.module.state_dict(), 'b': self.gen_b.module.state_dict()}, gen_name)
+            torch.save({'a': self.dis_a.module.state_dict(), 'b': self.dis_b.module.state_dict()}, dis_name)
+            torch.save({'gen': self.gen_opt.module.state_dict(), 'dis': self.dis_opt.module.state_dict()}, opt_name)
+        else:
+            torch.save({'a': self.gen_a.state_dict(), 'b': self.gen_b.state_dict()}, gen_name)
+            torch.save({'a': self.dis_a.state_dict(), 'b': self.dis_b.state_dict()}, dis_name)
+            torch.save({'gen': self.gen_opt.state_dict(), 'dis': self.dis_opt.state_dict()}, opt_name)
+
+
 
 
 class UNIT_Trainer(nn.Module):
